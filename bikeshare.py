4e65aad06aa25725bd87405ab719f3779f766a62
Nndweleni Sundani <nndwelenis@gmail.com>
2020-04-17 18:28:32 +0200

    update welcome message

--- bikeshare.py
+++ bikeshare.py
@@ -7,7 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-    # Function to filter the data we are interested in 
+    # Function to filter the data we are interested in
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -17,7 +17,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('-'*80)
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s have some fun exploring some US bikeshare data!')
 
     # get user input for city (chicago, new york city, washington). We used a while loop to handle invalid inputs
     city = input("\nPlease enter city name (chicago, new york city, washington): ").lower()
@@ -28,7 +28,7 @@ def get_filters():
     month = input("Please enter a month name in full (all, january, february, ... , june): ").lower()
     while month not in ['all', 'january', 'february', 'march', 'april', 'may', 'june']:
         month = input("Month name entered is invalid! Please enter another month name: ").lower()
-   
+
     # get user input for day of week (all, monday, tuesday, ... , sunday)
     day = input("Please enter a day of the week (all, monday, tuesday, ... , sunday): ").lower()
     while day not in ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
@@ -36,7 +36,7 @@ def get_filters():
 
     print('-'*80)
     return city, month, day
-    
+
 
 def load_data(city, month, day):
     """
@@ -59,7 +59,7 @@ def load_data(city, month, day):
     #Extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    
+
     # Filter by month if applicable
     if month != 'all':
         # Use the index of the months list to get the corresponding data
@@ -68,15 +68,15 @@ def load_data(city, month, day):
 
         #Filter by month to create a new dataframe
         df = df[df['month'] == month]
-    
+
     # Filter by day of week if applicable
     if day != 'all':
         #Filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
 
     # Returns the selected file as a dataframe (df) with relevant columns
-    return df    
-    
+    return df
+
 # Function to calculate all the time-related statistics for the chosen data
 def time_stats(df):
     """Displays statistics on the most frequent times of travel.
@@ -104,14 +104,14 @@ def time_stats(df):
 
     #Uses mode method to find the most popular hour
     popular_hour = df['hour'].mode()[0]
-    
+
     print(f"\nMost Popular Start Hour: {popular_hour}")
 
     #Prints the time taken to perform the calculation
     print(f"\nThis took {(time.time() - start_time)} seconds.")
     print('-'*80)
 
-    # Function to calculate station related statistics    
+    # Function to calculate station related statistics
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
@@ -126,7 +126,7 @@ def station_stats(df):
     print("The most commonly used end station is: {}".format(
         df['End Station'].mode().values[0]))
 
-    # Display most frequent combination of start station and end station trip. We start by creating a new table, which is a combination of 'start station' and 'end station'. 
+    # Display most frequent combination of start station and end station trip. We start by creating a new table, which is a combination of 'start station' and 'end station'.
     df['routes'] = df['Start Station']+ " " + df['End Station']
     print("The most common start and end station combination is: {}".format(
         df['routes'].mode().values[0]))
@@ -140,7 +140,7 @@ def trip_duration_stats(df):
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
-    
+
 #Convert the Start Time and End Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['duration'] = df['End Time'] - df['Start Time']
@@ -161,10 +161,10 @@ def trip_duration_stats(df):
 
 # Displaying the user_statisics
 def user_stats(df, city):
-    
+
     """Displays statistics on bikeshare users."""
-    
-        
+
+
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
@@ -173,7 +173,7 @@ def user_stats(df, city):
     print(df['User Type'].value_counts())
 
     # Displaying the counts of gender
-    
+
     if  city != 'washington':
         print("\nThe following are the counts in terms of gender:")
         print(df['Gender'].value_counts())
@@ -194,14 +194,14 @@ def user_stats(df, city):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-    
+
 def main():
     while True:
-        city, month, day = get_filters() 
+        city, month, day = get_filters()
         df = load_data(city, month, day)
-        
+
         time_stats(df)
-        station_stats(df) 
+        station_stats(df)
         trip_duration_stats(df)
         user_stats(df, city)
 
@@ -211,13 +211,7 @@ def main():
 
 if __name__ == "__main__":
 	main()
-    
+
 #REFERENCES
 #1 https://stackoverflow.com/questions/48400250/subtract-two-objects-data-type-in-python
 #2 https://stackoverflow.com/questions/26763344/convert-pandas-column-to-datetime
-
-
-
-    
-    
-    
